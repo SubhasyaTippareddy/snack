cd564507844297135b9687a571da8321afe8471b
SubhasyaTippareddy <subhasya999@gmail.com>
2021-02-23 20:13:38 +0530

    sub

---
+++ menu/forms.py
@@ -0,0 +1,9 @@
+# from django import forms
+# from menu.models import Student
+# from django.contrib.auth.models import User
+
+# class StudentForm(forms.ModelForm):
+#     password=forms.CharField(widget=forms.PasswordInput())
+#     class Meta():
+#         model = Student
+#         fields = ('username','password')
