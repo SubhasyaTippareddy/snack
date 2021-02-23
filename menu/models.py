c6a27a1e2502476e7d19704ea47a9de437ff02f3
Arvindrv2 <arvindrv2@gmail.com>
2021-02-23 11:32:51 +0530

    needs change

--- menu/models.py
+++ menu/models.py
@@ -1,19 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.conf import settings
-class User(AbstractUser):
-    is_student=models.BooleanField(default=False)
-    is_cook=models.BooleanField(default=False)
-class Student(models.Model):
-    user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    sid= models.CharField(max_length=8,blank=False,primary_key=True)
-    sname=models.CharField(max_length=50,blank=False)
-    password=models.CharField(max_length=20,blank=False)
-    def __str__(self):
-        return self.user.sname
-class Cook(models.Model):
-    user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    cname=models.CharField(max_length=25,blank=False)
-    email=models.EmailField(max_length=100,blank=False)
-    def __str__(self):
-        return self.user.cname
+# from django.contrib.auth.models import AbstractUser
+# from django.conf import settings
+# class User(AbstractUser):
+#     is_student=models.BooleanField(default=False)
+#     is_cook=models.BooleanField(default=False)
+# class Student(models.Model):
+#     user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+#     sid= models.CharField(max_length=8,blank=False,primary_key=True)
+#     sname=models.CharField(max_length=50,blank=False)
+#     password=models.CharField(max_length=20,blank=False)
+#     def __str__(self):
+#         return self.user.sname
+# class Cook(models.Model):
+#     user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+#     cname=models.CharField(max_length=25,blank=False)
+#     email=models.EmailField(max_length=100,blank=False)
+#     def __str__(self):
+#         return self.user.cname
