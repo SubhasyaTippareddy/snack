99bb469b136f7584dcc47327d933f9121f2a4dd3
charan1967 <charanvasabathula@gmail.com>
2021-02-23 20:58:06 +0530

    a

--- menu/models.py
+++ menu/models.py
@@ -1,16 +1,18 @@
 from django.db import models
+#from django.contrib.auth.models import User
 # from django.contrib.auth.models import AbstractUser
 # from django.conf import settings
-# class User(AbstractUser):
+#class User(AbstractUser):
+    
 #     is_student=models.BooleanField(default=False)
 #     is_cook=models.BooleanField(default=False)
 # class Student(models.Model):
-#     user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-#     sid= models.CharField(max_length=8,blank=False,primary_key=True)
+#     user=models.OneToOneField(User,on_delete=models.CASCADE)
+#     username= models.CharField(max_length=8,blank=False,primary_key=True)
 #     sname=models.CharField(max_length=50,blank=False)
 #     password=models.CharField(max_length=20,blank=False)
 #     def __str__(self):
-#         return self.user.sname
+#         return self.user.username
 # class Cook(models.Model):
 #     user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
 #     cname=models.CharField(max_length=25,blank=False)
