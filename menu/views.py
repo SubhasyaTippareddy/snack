e3a4eaf28789cbb49b0e4e8459757a6ef987f13f
Arvindrv2 <arvindrv2@gmail.com>
2021-02-24 12:57:07 +0530

    idk man

--- menu/views.py
+++ menu/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 #def home(request):
     #return HttpResponse("Hey this is home!")
 def menu(request):
-    return render(request,'./index.html')
+    return render(request,'./index.html') #./ to go to the root and index.html is the file name
 def studentLogin(request):
     return render(request,'./Student.html')
 def chefLogin(request):
